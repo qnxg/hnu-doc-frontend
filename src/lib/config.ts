ce4d9ff983264c7ec1ff031e6bb47a6070156223
Twisuki <Twisuki@outlook.com>
2026-01-28 19:29:54 +0800

    :poop: 移除 .env, 改为硬编码的URL

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,3 @@
-import process from "node:process"
-
 export default {
-  BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL || "http://127.0.0.1:4523/m1/6049750-5739759-default",
+  BACKEND_URL: "http://127.0.0.1:4523/m1/6049750-5739759-default",
 }
