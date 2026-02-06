5dddd9678bf65a84b330be2a551dd2583a695d52
Twisuki <Twisuki@outlook.com>
2026-02-06 23:58:31 +0800

    :sparkles: 首页添加加载中提示

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react"
 import { getCollections } from "@/src/apis/collection.server"
 import CollectionTable from "@/src/components/dashboard/table"
 
@@ -11,7 +12,14 @@ export default async function DashboardPage() {
       <div className="w-full text-center text-3xl">
         常用试卷集
       </div>
-      <CollectionTable collections={collections} />
+      <Suspense fallback={(
+        <div className="flex items-center justify-center py-12 text-muted-foreground">
+          <h1 className="text-xl font-semibold">加载中...</h1>
+        </div>
+      )}
+      >
+        <CollectionTable collections={collections} />
+      </Suspense>
     </div>
   )
 }
